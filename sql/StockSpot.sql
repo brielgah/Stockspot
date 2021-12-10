4891bfe16f25ff346a4a1103b875479ce5a1f0df
jluisv987 <jluisv987@gmail.com>
2021-12-09 23:12:52 -0600

    BD actualizada

--- sql/StockSpot.sql
+++ sql/StockSpot.sql
@@ -1,65 +1,236 @@
-create database StockSpot;
-use StockSpot;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 10-12-2021 a las 05:47:27
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.0.13
 
-create table Empleado(
-	ID_Empleado integer AUTO_INCREMENT,
-	Nombre varchar(45) not null,
-    Edad integer not null,
-    Sexo varchar(1) not null,
-    Correo_Electronico varchar(40) not null,
-    Sueldo double not null,
-    Foto longblob,
-    Contraseña varchar(16),
-    primary key(ID_Empleado)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-create table Producto(
-	ID_Producto integer auto_increment not null,
-    Nombre varchar(45) not null,
-    Precio double not null,
-    Descripcion varchar(100),
-    Stock integer not null,
-    primary key(ID_Producto)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `StockSpot`
+--
 
+-- --------------------------------------------------------
 
-create table Venta(
-	ID integer auto_increment not null,
-    ID_Empleado integer not null,
-    Monto double not null,
-    Fecha date not null,
-    primary key (ID), 
-    foreign key (ID_Empleado) references Empleado(ID_Empleado)
-);
+--
+-- Estructura de tabla para la tabla `Compra`
+--
 
-create table Proveedor(
-	ID_Proveedor integer auto_increment not null,
-    Nombre varchar(45),
-	Correo_Electronico varchar(35),
-	primary key(ID_Proveedor)
-);
-create table Compra(
-	ID integer auto_increment not null,
-    ID_Empleado integer not null,
-    ID_Proveedor integer not null,
-    Monto double not null,
-    Fecha date not null,
-    primary key (ID), 
-    foreign key (ID_Empleado) references Empleado(ID_Empleado),
-     foreign key (ID_Proveedor) references Proveedor(ID_Proveedor)
-);
+CREATE TABLE `Compra` (
+  `ID` int(11) NOT NULL,
+  `ID_Empleado` int(11) NOT NULL,
+  `ID_Proveedor` int(11) NOT NULL,
+  `Monto` double NOT NULL,
+  `Fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table Orden(
-	ID integer not null,
-    ID_Producto integer not null,
-    Cantidad integer not null,
-	
-    foreign key(ID) references Venta(ID),
-	foreign key(ID) references Compra(ID),
-	foreign key(ID_Producto) references Producto(ID_Producto)
-    
-);
+-- --------------------------------------------------------
 
-INSERT INTO `Empleado` (`ID_Empleado`, `Nombre`, `Edad`, `Sexo`, `Correo_Electronico`, `Sueldo`, `Foto`, `Contraseña`) VALUES (NULL, 'Administrador', '20', 'M', 'admin@admin.com', '1000000', NULL, 'admin')
+--
+-- Estructura de tabla para la tabla `Empleado`
+--
+
+CREATE TABLE `Empleado` (
+  `ID_Empleado` int(11) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Edad` int(11) NOT NULL,
+  `Sexo` varchar(1) NOT NULL,
+  `Correo_Electronico` varchar(40) NOT NULL,
+  `Sueldo` double NOT NULL,
+  `Foto` longblob DEFAULT NULL,
+  `Contraseña` varchar(16) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `Empleado`
+--
+
+INSERT INTO `Empleado` (`ID_Empleado`, `Nombre`, `Edad`, `Sexo`, `Correo_Electronico`, `Sueldo`, `Foto`, `Contraseña`) VALUES
+(2, 'Administrador', 20, 'M', 'admin@admin.com', 1000000, NULL, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Orden`
+--
+
+CREATE TABLE `Orden` (
+  `ID` int(11) NOT NULL,
+  `ID_Producto` int(11) NOT NULL,
+  `Cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Producto`
+--
+
+CREATE TABLE `Producto` (
+  `ID_Producto` int(11) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Precio` double NOT NULL,
+  `Descripcion` varchar(100) DEFAULT NULL,
+  `Stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `Producto`
+--
+
+INSERT INTO `Producto` (`ID_Producto`, `Nombre`, `Precio`, `Descripcion`, `Stock`) VALUES
+(1, 'Agua Alcalina', 55.5, 'Agua alcalina de 1500 mL, Ph. 8.8', 5),
+(2, 'Galletas', 12, 'Galletas de mantequilla ', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Proveedor`
+--
+
+CREATE TABLE `Proveedor` (
+  `ID_Proveedor` int(11) NOT NULL,
+  `Nombre` varchar(45) DEFAULT NULL,
+  `Numero` varchar(10) NOT NULL,
+  `Compania` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `Proveedor`
+--
+
+INSERT INTO `Proveedor` (`ID_Proveedor`, `Nombre`, `Numero`, `Compania`) VALUES
+(1, 'Miguel', '3334567890', 'Marinela'),
+(2, 'José', '1234567890', 'Pepsico');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Venta`
+--
+
+CREATE TABLE `Venta` (
+  `ID` int(11) NOT NULL,
+  `ID_Empleado` int(11) NOT NULL,
+  `Monto` double NOT NULL,
+  `Fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Compra`
+--
+ALTER TABLE `Compra`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `ID_Empleado` (`ID_Empleado`),
+  ADD KEY `ID_Proveedor` (`ID_Proveedor`);
+
+--
+-- Indices de la tabla `Empleado`
+--
+ALTER TABLE `Empleado`
+  ADD PRIMARY KEY (`ID_Empleado`);
+
+--
+-- Indices de la tabla `Orden`
+--
+ALTER TABLE `Orden`
+  ADD KEY `ID` (`ID`),
+  ADD KEY `ID_Producto` (`ID_Producto`);
+
+--
+-- Indices de la tabla `Producto`
+--
+ALTER TABLE `Producto`
+  ADD PRIMARY KEY (`ID_Producto`);
+
+--
+-- Indices de la tabla `Proveedor`
+--
+ALTER TABLE `Proveedor`
+  ADD PRIMARY KEY (`ID_Proveedor`);
+
+--
+-- Indices de la tabla `Venta`
+--
+ALTER TABLE `Venta`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `ID_Empleado` (`ID_Empleado`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Compra`
+--
+ALTER TABLE `Compra`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `Empleado`
+--
+ALTER TABLE `Empleado`
+  MODIFY `ID_Empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `Producto`
+--
+ALTER TABLE `Producto`
+  MODIFY `ID_Producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `Proveedor`
+--
+ALTER TABLE `Proveedor`
+  MODIFY `ID_Proveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `Venta`
+--
+ALTER TABLE `Venta`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `Compra`
+--
+ALTER TABLE `Compra`
+  ADD CONSTRAINT `Compra_ibfk_1` FOREIGN KEY (`ID_Empleado`) REFERENCES `Empleado` (`ID_Empleado`),
+  ADD CONSTRAINT `Compra_ibfk_2` FOREIGN KEY (`ID_Proveedor`) REFERENCES `Proveedor` (`ID_Proveedor`);
+
+--
+-- Filtros para la tabla `Orden`
+--
+ALTER TABLE `Orden`
+  ADD CONSTRAINT `Orden_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `Venta` (`ID`),
+  ADD CONSTRAINT `Orden_ibfk_2` FOREIGN KEY (`ID`) REFERENCES `Compra` (`ID`),
+  ADD CONSTRAINT `Orden_ibfk_3` FOREIGN KEY (`ID_Producto`) REFERENCES `Producto` (`ID_Producto`);
+
+--
+-- Filtros para la tabla `Venta`
+--
+ALTER TABLE `Venta`
+  ADD CONSTRAINT `Venta_ibfk_1` FOREIGN KEY (`ID_Empleado`) REFERENCES `Empleado` (`ID_Empleado`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
